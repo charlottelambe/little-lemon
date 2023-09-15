b88a5c83c4b999cdfd1b83323d085423b441f8c5
charlottelambe <charlottelambe@hotmail.com>
2023-09-15 22:35:51 +0100

    Adding CSS grid to the Homepage Hero

--- little-lemon/src/App.js
+++ little-lemon/src/App.js
@@ -9,7 +9,7 @@ import Footer from './components/Footer';
 function App() {
   return (
     <>
-      <Header/>
+      <Header />
       <Hero/>
       <Specials/>
       <Testimonials/>
--- little-lemon/src/components/pages/Home/Hero.js
+++ little-lemon/src/components/pages/Home/Hero.js
@@ -1,15 +1,20 @@
+import './Hero.css';
 import restaurantFood from '../../../images/restaurant-food.png';
 
 const Hero = () => {
     return (
 
-    <hero>
-       <h1>Little Lemon</h1> 
-       <h2>Chicago</h2>
-       <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
-       <button>Reserve a Table</button>
-       <img src={restaurantFood} alt="Food at the Little Lemon Restaurant" />
-    </hero>
+    <div class="hero-container">
+        <div class="hero-text">
+            <h1>Little Lemon</h1>
+            <h2>Chicago</h2>
+            <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+            <button>Reserve a Table</button>
+        </div>
+        <div class="hero-image">
+            <img src={restaurantFood} alt="Food at the Little Lemon Restaurant" />
+        </div>
+    </div>
 );
 };
 
